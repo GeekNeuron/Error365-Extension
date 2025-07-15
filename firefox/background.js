423dbfe8f490e078dabd364c06318de578fa41ae
Geek Neuron <geekneuron@gmail.com>
2025-07-15 11:43:21 +0330

    Update background.js

--- firefox/background.js
+++ firefox/background.js
@@ -1,6 +1,9 @@
 browser.webNavigation.onErrorOccurred.addListener(async (details) => {
   if (details.frameId === 0) {
-    await browser.storage.local.set({ lastErrorCode: details.error });
+    const errorCode = details.error;
+    await browser.storage.local.set({ lastErrorCode: errorCode });
+
+    browser.runtime.sendMessage({ type: "ERROR_SET", code: errorCode });
 
     await browser.action.setBadgeText({ tabId: details.tabId, text: "!" });
     await browser.action.setBadgeBackgroundColor({ tabId: details.tabId, color: "#D32F2F" });
@@ -10,11 +13,15 @@ browser.webNavigation.onErrorOccurred.addListener(async (details) => {
 browser.webNavigation.onCompleted.addListener(async (details) => {
   if (details.frameId === 0) {
     if (details.statusCode >= 400) {
-      await browser.storage.local.set({ lastErrorCode: details.statusCode.toString() });
+      const errorCode = details.statusCode.toString();
+      await browser.storage.local.set({ lastErrorCode: errorCode });
+
+      browser.runtime.sendMessage({ type: "ERROR_SET", code: errorCode });
 
       await browser.action.setBadgeText({ tabId: details.tabId, text: "!" });
       await browser.action.setBadgeBackgroundColor({ tabId: details.tabId, color: "#D32F2F" });
     } else {
+      await browser.storage.local.remove('lastErrorCode');
       await browser.action.setBadgeText({ tabId: details.tabId, text: "" });
     }
   }
